b4aa75da9d7faacbf7022192f6ccfdbbb3fafcef
Akhila sajeevan <akhilasajeevan@gmail.com>
2024-03-06 14:07:52 +0530

    first commit

---
+++ calculator1.js
@@ -0,0 +1,15 @@
+function calculator1(operator,value){
+    let     result;    
+    
+    switch(operator){
+case "+":result=value+value;
+break;
+case "-":result=value-value;
+break;
+case "/":result=value/value;
+break;
+case "*":result=value*value;
+break;
+}
+}
+calculator1()
